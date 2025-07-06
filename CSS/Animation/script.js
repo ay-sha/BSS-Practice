de67940f95f449514b4ba20e3cf34ee31c3b2f00
Aysha <ayshaamin680@gmail.com>
2025-07-06 13:51:47 +0600

    CSS Done

--- CSS/Animation/script.js
+++ CSS/Animation/script.js
@@ -1,18 +1,19 @@
-// const btn = document.querySelector('#btn2');
-//     const container = document.querySelector('.container3')
-//     btn.addEventListener('click', function () {
-//         container.classList.toggle('dark');
-//     });
-
+// Toggle Button
 const toggle = document.querySelector('.mobile-menu-toggle');
-    const mobileMenu = document.querySelector('.mobile-menu-items');
+const btn = document.querySelector('#btn2');
+const container = document.querySelector('.container3')
+// btn.addEventListener('click', function () {
+//     container.classList.toggle('dark');
+// });
 
-    if (toggle && mobileMenu) {
-        toggle.addEventListener('click', function () {
-            if (mobileMenu.classList.contains('active')) {
-                mobileMenu.classList.remove('active');
-            } else {
-                mobileMenu.classList.add('active');
-            }
-        });
-    }
+// Hamburger
+const mobileMenu = document.querySelector('.mobile-menu-items');
+if (toggle && mobileMenu) {
+    toggle.addEventListener('click', function () {
+        if (mobileMenu.classList.contains('active')) {
+            mobileMenu.classList.remove('active');
+        } else {
+            mobileMenu.classList.add('active');
+        }
+    });
+}
